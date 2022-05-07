71ad55717df07904a9503c40db171b641482982a
joeriddles <joeriddles10@gmail.com>
2022-05-07 09:43:30 -0700

    Add feedback for first two projects

--- JBsFlightTracker/flight_data.py
+++ JBsFlightTracker/flight_data.py
@@ -1,3 +1,20 @@
+# TODO: Consider using `dataclasses` or `attrs`:
+# import dataclasses
+# import datetime
+# from decimal import Decimal
+
+# @dataclasses.dataclass
+# class FlightData:
+#     price: Decimal
+#     origin_city: str
+#     origin_airport: str
+#     destination_city: str
+#     destination_airport: str
+#     out_date: datetime.date
+#     return_date: datetime.date
+#     stop_overs: int = 0
+#     var_city: str = ""
+
 class FlightData:
 
     def __init__(self, price, origin_city, origin_airport, destination_city, destination_airport, out_date, return_date):
